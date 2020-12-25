bd1690c9950ee662f922128c75d0af254f7afb8c
jneo8 <jneo8lin@gmail.com>
2020-12-25 15:43:43 +0800

    feat(2020-day3-part2): Solved 2020 day3 part2

--- 2020/day3.py
+++ 2020/day3.py
@@ -1,5 +1,7 @@
 """AdventOfCode 2020 Day3."""
 from itertools import count, islice
+from functools import reduce
+from operator import mul
 
 
 def get_input():
@@ -21,5 +23,21 @@ def part1(trees, right=3, down=1):
     return sum(row[col % len(row)] == "#" for (row, col) in zip(rows, cols))
 
 
+def part2(trees):
+    """Part 2."""
+    counts = (
+        part1(trees, r, d)
+        for r, d in [
+            (1, 1),
+            (3, 1),
+            (5, 1),
+            (7, 1),
+            (1, 2),
+        ]
+    )
+    return reduce(mul, counts)
+
+
 if __name__ == "__main__":
     print(part1(get_input()))
+    print(part2(get_input()))
