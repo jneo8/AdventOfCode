bdc044f3c16df0b737b9dfb9afa69128c1d207f1
jneo8 <jneo8lin@gmail.com>
2021-02-16 23:07:10 +0800

    feat(2020-day23-part2): Solved

--- 2020/day23.py
+++ 2020/day23.py
@@ -33,6 +33,37 @@ def playing(cups: List[int], move: int):
     return list(q)
 
 
+def move(cups, moves, pad):
+    nex = [i + 1 for i in range(pad + 1)]
+    for i, label in enumerate(cups[:-1]):
+        nex[label] = cups[i + 1]
+    head = cups[0]
+    if pad > len(cups):
+        nex[-1] = head
+        nex[cups[-1]] = max(cups) + 1
+    else:
+        nex[cups[-1]] = head
+
+    for i in range(moves):
+        rem = nex[head]
+        nex[head] = nex[nex[nex[rem]]]
+        allrem = rem, nex[rem], nex[nex[rem]]
+
+        dest = head - 1 if head > 1 else pad
+        while dest in allrem:
+            dest = pad if dest == 1 else dest - 1
+
+        nex[nex[nex[rem]]] = nex[dest]
+        nex[dest] = rem
+
+        head = nex[head]
+
+    cup = nex[1]
+    while cup != 1:
+        yield cup
+        cup = nex[cup]
+
+
 def part1(puzzle_input: str, move: int):
     """Part1."""
     cups = playing([int(n) for n in list(puzzle_input)], move)
@@ -41,6 +72,15 @@ def part1(puzzle_input: str, move: int):
     return "".join(str(n) for n in cups)
 
 
+def part2(puzzle_input: str):
+    """Part2."""
+    cups = list(map(int, puzzle_input))
+    m = move(cups, 10000000, 1000000)
+    return next(m) * next(m)
+
+
 if __name__ == "__main__":
     assert part1(test_input_1, 10) == "92658374"
     print(f"Part1: {part1(puzzle_input, 100)}")
+    assert part2("389125467") == 149245887792
+    print(f"Part2: {part2(puzzle_input)}")
